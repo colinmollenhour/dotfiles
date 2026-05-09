747dc1547ba23a3cfd197d465046782ef64ca2d8
Colin Mollenhour <colin@mollenhour.com>
2026-05-09 00:10:13 +0000

    Fix snip-upload to use bun

--- bin/snip-upload.ts
+++ bin/snip-upload.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 import { createHash, createHmac, randomBytes } from 'node:crypto'
 import { readFile, stat, unlink, writeFile } from 'node:fs/promises'
 import { spawnSync } from 'node:child_process'
